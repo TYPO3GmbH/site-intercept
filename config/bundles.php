5524627491e7b979b09bfe367b4e61eff454d4b6
Christian Kuhn <lolli@schwarzbu.ch>
2018-11-21 11:37:08 +0100

    [TASK] Require eightpoints/guzzle-bundle
    composer require eightpoints/guzzle-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
 ];
