ad73eb6fac5eb9798e5496f7e890d5ef5cce4f28
Christian Kuhn <lolli@schwarzbu.ch>
2018-11-23 18:06:34 +0100

    [TASK] Remove symfony/maker-bundle
    composer remove symfony/maker-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
