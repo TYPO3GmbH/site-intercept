f513b4b41ee653a2585ca2b4541619d94975124a
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-06 17:01:58 +0100

    [TASK] Have fontawesome
    yarn add --dev @fortawesome/fontawesome-free

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,7 @@ var $ = require('jquery');
 
 require('bootstrap');
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
