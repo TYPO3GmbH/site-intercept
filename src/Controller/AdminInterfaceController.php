9b88b907388d8998bf2e3a4af3de5e0c63b4d64f
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-06 15:16:37 +0100

    [TASK] admin route with base template

--- src/Controller/AdminInterfaceController.php
+++ src/Controller/AdminInterfaceController.php
@@ -13,6 +13,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Handle the web admin interface
@@ -26,6 +27,6 @@ class AdminInterfaceController extends AbstractController
      */
     public function index(Request $request): Response
     {
-        return Response::create();
+        return $this->render('base.html.twig');
     }
 }
