874238c07b364f2fb209b8c859b359477fb4b9b8
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-06 14:24:10 +0100

    [TASK] Basic admin web controller

---
+++ src/Controller/AdminInterfaceController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/intercept.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Handle the web admin interface
+ */
+class AdminInterfaceController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_index")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        return Response::create();
+    }
+}
