35000f24ce2e8b196e0f47ee329596f96b4fa606
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-11 18:27:56 +0100

    [TASK] Add a testing related double bundle and use in tests
    Symfony does not easily allow substituting single (third-party)
    dependencies in functional tests. One reason is that each request
    creates a new kernel and container, which is obvious for
    $client->request(), but also happens implicitly if a test does
    something like $form->submit(). Tests like these do multiple
    requests: First one to render a form, second to submit form data.
    
    If such a form submit triggers a third party call - in our case
    to bamboo for example, a test should be able to mock that call.
    But that's not easily possible. Searching for this a casual answer
    is "you're doing it wrong" and those who ask for the valid case
    that for instance a guzzle call to a third party should be mockable
    are not answered.
    
    This solution is inspired by
    https://github.com/docteurklein/TestDoubleBundle
    but allows injecting object prophecies prepared by the test itself
    for as many calls as needed AND having the prophecy available in
    the test directly.

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    App\Bundle\TestDoubleBundle::class => ['test' => true],
 ];
---
+++ tests/Functional/AdminInterfaceControllerTest.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types = 1);
+namespace App\Tests\Functional;
+
+use App\Bundle\TestDoubleBundle;
+use App\Client\BambooClient;
+use GuzzleHttp\Psr7\Response;
+use Prophecy\Argument;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminInterfaceControllerTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function indexPageIsRendered()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/admin');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegExp('/Intercept web admin interface/', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function bambooCoreFormIsRendered()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/admin/bamboo');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegExp('/Trigger bamboo builds/', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function bambooCoreCanBeTriggered()
+    {
+        // Bamboo client double for the first request
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $this->prophesize(BambooClient::class));
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/bamboo');
+
+        // Bamboo client double for the second request
+        $bambooClientProphecy = $this->prophesize(BambooClient::class);
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $bambooClientProphecy);
+        $bambooClientProphecy->post(Argument::cetera())->willReturn(
+            new Response(200, [], json_encode(['buildResultKey' => 'CORE-GTC-123456']))
+        );
+
+        // Get the rendered form, feed it with some data and submit it
+        $form = $crawler->selectButton('Trigger master')->form();
+        $form['bamboo_trigger_form[change]'] = '58920';
+        $form['bamboo_trigger_form[set]'] = 3;
+        $client->submit($form);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        // The build key is shown
+        $this->assertRegExp('/CORE-GTC-123456/', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function bambooCoreTriggeredReturnsErrorIfBambooClientDoesNotCreateBuild()
+    {
+        // Bamboo client double for the first request
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $this->prophesize(BambooClient::class));
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/bamboo');
+
+        // Bamboo client double for the second request
+        $bambooClientProphecy = $this->prophesize(BambooClient::class);
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $bambooClientProphecy);
+        // Simulate bamboo did not trigger a build - buildResultKey missing in response
+        $bambooClientProphecy->post(Argument::cetera())->willReturn(
+            new Response(200, [], json_encode([]))
+        );
+
+        // Get the rendered form, feed it with some data and submit it
+        $form = $crawler->selectButton('Trigger master')->form();
+        $form['bamboo_trigger_form[change]'] = '58920';
+        $form['bamboo_trigger_form[set]'] = 3;
+        $client->submit($form);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegExp('/Bamboo trigger not successful/', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function bambooCoreTriggeredReturnsErrorIfBrokenFormIsSubmitted()
+    {
+        // Bamboo client double for the first request
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $this->prophesize(BambooClient::class));
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/bamboo');
+
+        // Bamboo client double for the second request
+        $bambooClientProphecy = $this->prophesize(BambooClient::class);
+        TestDoubleBundle::addProphecy('App\Client\BambooClient', $bambooClientProphecy);
+        $bambooClientProphecy->post(Argument::cetera())->willReturn(
+            new Response(200, [], json_encode([]))
+        );
+
+        $form = $crawler->selectButton('Trigger master')->form();
+        // Empty change is not allowed
+        $form['bamboo_trigger_form[change]'] = '';
+        $form['bamboo_trigger_form[set]'] = 3;
+        $client->submit($form);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegExp('/Could not determine a changeId/', $client->getResponse()->getContent());
+    }
+}
