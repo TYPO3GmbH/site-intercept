e88b707216af85d7f1bc8b141e081af546bc44b4
Woeler <woeler@woeler.eu>
2019-05-10 13:36:51 +0200

    [BUGIFX] Add lifecyclecallbacks orm annotation

--- src/Entity/DocumentationJar.php
+++ src/Entity/DocumentationJar.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\DocumentationJarRepository")
+ * @ORM\HasLifecycleCallbacks
  * @codeCoverageIgnore
  */
 class DocumentationJar
