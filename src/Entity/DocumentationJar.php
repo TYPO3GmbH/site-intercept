4a55237eca61e8bb91db596503a7d7662ee2a31b
Woeler <woeler@woeler.eu>
2019-05-10 14:25:07 +0200

    [TASK] Force DateTimes to be UTC via Doctrine

--- src/Entity/DocumentationJar.php
+++ src/Entity/DocumentationJar.php
@@ -138,12 +138,11 @@ class DocumentationJar
      */
     public function updatedTimestamps(): void
     {
-        $dtz = new \DateTimeZone('UTC');
         if ($this->getCreatedAt() === null) {
-            $this->setCreatedAt(new \DateTime('now', $dtz));
+            $this->setCreatedAt(new \DateTime('now'));
         }
         if ($this->getLastRenderedAt() === null) {
-            $this->setLastRenderedAt(new \DateTime('now', $dtz));
+            $this->setLastRenderedAt(new \DateTime('now'));
         }
         if ($this->getTargetBranchDirectory() === null) {
             $this->targetBranchDirectory = $this->getBranch();
