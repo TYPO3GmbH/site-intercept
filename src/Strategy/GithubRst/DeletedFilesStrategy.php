d42fd85cd7d2c05d982d3879b3bfadc27dc955e3
Andreas Fernandez <andreas.fernandez@typo3.com>
2022-07-11 15:29:43 +0200

    [TASK][COMMUN-405] Render diff of changed files

---
+++ src/Strategy/GithubRst/DeletedFilesStrategy.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3g/intercept.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Strategy\GithubRst;
+
+use App\Client\GeneralClient;
+use App\Extractor\GithubPushEventForCore;
+
+class DeletedFilesStrategy implements StrategyInterface
+{
+    private GeneralClient $client;
+
+    public function __construct(GeneralClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function match(string $type): bool
+    {
+        return $type === 'removed';
+    }
+
+    public function getFromGithub(GithubPushEventForCore $pushEvent, string $filename): string
+    {
+        $url = sprintf('https://raw.githubusercontent.com/%s/%s/%s', $pushEvent->repositoryFullName, $pushEvent->commit['id'], $filename);
+        $response = $this->client->request('GET', $url);
+
+        return (string)$response->getBody();
+    }
+
+    public function formatResponse(string $response): string
+    {
+        return sprintf("```rst\n%s\n```\n", $response);
+    }
+}
---
+++ src/Strategy/GithubRst/ModifiedFilesStrategy.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3g/intercept.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Strategy\GithubRst;
+
+use App\Client\GeneralClient;
+use App\Extractor\GithubPushEventForCore;
+
+class ModifiedFilesStrategy implements StrategyInterface
+{
+    private GeneralClient $client;
+
+    public function __construct(GeneralClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function match(string $type): bool
+    {
+        return $type === 'modified';
+    }
+
+    public function getFromGithub(GithubPushEventForCore $pushEvent, string $filename): string
+    {
+        $url = sprintf('https://api.github.com/repos/%s/compare/%s...%s', $pushEvent->repositoryFullName, $pushEvent->beforeCommitId, $pushEvent->afterCommitId);
+        $response = $this->client->request('GET', $url);
+        $compare = json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        $files = $compare['files'] ?? [];
+
+        foreach ($files as $file) {
+            if ($file['filename'] === $filename) {
+                return $file['patch'];
+            }
+        }
+
+        return '';
+    }
+
+    public function formatResponse(string $response): string
+    {
+        return sprintf("```diff\n%s\n```\n", $response);
+    }
+}
