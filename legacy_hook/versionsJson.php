de30751ff63951d50668f99a1c91f786ea6c209d
lina.wolf <lwolf@w-commerce.de>
2024-03-02 07:56:36 +0100

    [FEATURE] Return Versions as JSON
    This enables the documentation Team to generate the HTML ourselves if changes are desired

---
+++ legacy_hook/versionsJson.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/intercept-legacy-hook.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\DocumentationVersions;
+use App\ResponseEmitter;
+use GuzzleHttp\Psr7\ServerRequest;
+
+$response = (new DocumentationVersions(ServerRequest::fromGlobals()))->getVersions('JSON');
+new ResponseEmitter($response);
