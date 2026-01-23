af0ad5fb0d65b2d5ac5d55e5e76a5947d849268f
Garvin Hicking <blog@garv.in>
2026-01-23 08:35:53 +0100

    [TASK] Allow to redirect to CMS core manuals without "cms-" prefix
    This allows to parse theme-camino links, which is not called
    "cms-theme-camino".

--- legacy_hook/src/DocumentationLinker.php
+++ legacy_hook/src/DocumentationLinker.php
@@ -36,9 +36,11 @@ use T3Docs\VersionHandling\Typo3VersionMapping;
  * linkToDocs.php?shortcode=georgringer-news:start@main
  * -> forwards to: https://docs.typo3.org/p/georgringer/news/en-us/Index.html#start
  *
- * Also, all TYPO3 core extensions can be resolved via "typo3-cms-XXX" prefixing:
+ * Also, all TYPO3 core extensions can be resolved via "typo3-(cms-)?XXX" prefixing:
  * linkToDocs.php?shortcode=typo3-cms-seo:introduction@main
  * -> forwards to: https://docs.typo3.org/c/typo3/cms-seo/main/en-us/Introduction/Index.html#introduction
+ * linkToDocs.php?shortcode=typo3-theme-camino:introduction@main
+ * * -> forwards to: https://docs.typo3.org/c/typo3/theme-camino/main/en-us/Introduction/Index.html#introduction
  *
  * The parts of 'shortcode' URI GET/POST param 'shortcode' are:
  *
@@ -81,7 +83,7 @@ use T3Docs\VersionHandling\Typo3VersionMapping;
  * https://docs.typo3.org/m/XXX/main/en-us/objects.inv.json
  *
  * Core TYPO3 Extension manuals:
- * https://docs.typo3.org/c/typo3/cms-XXX/main/en-us/objects.inv.json
+ * https://docs.typo3.org/c/typo3/(cms-)?XXX/main/en-us/objects.inv.json
  *
  * Additional TYPO3 Manuals:
  * https://docs.typo3.org/other/typo3/XXX/main/en-us/objects.inv.json
@@ -257,7 +259,10 @@ final readonly class DocumentationLinker
     {
         $useCoreVersionResolving = true;
         if (preg_match('/^typo3[\/-](cms-[0-9a-z\-]+)$/i', $repository, $repositoryParts)) {
-            // CASE: TYPO3 core manuals
+            // CASE: TYPO3 core manuals with "cms-" prefix
+            $entrypoint = 'https://docs.typo3.org/c/typo3/' . strtolower($repositoryParts[1]) . '/{typo3_version}/en-us/';
+        } elseif (preg_match('/^typo3[\/-]([0-9a-z\-]+)$/i', $repository, $repositoryParts)) {
+            // CASE: TYPO3 core manuals without "cms-prefix" (like theme-camino)
             $entrypoint = 'https://docs.typo3.org/c/typo3/' . strtolower($repositoryParts[1]) . '/{typo3_version}/en-us/';
         } elseif ($inventory = DefaultInventories::tryFrom($repository)) {
             // CASE: Official TYPO3 Documentation with known inventories. Provides "{typo3_version}" internally
